6a9d6fc3264b88389ea4a122380eb754d9d0b578
Ariadna <Ariadna@192.168.1.43>
2020-01-22 21:05:55 +0000

    Create the image viewer

---
+++ src/imageviewer_1/Main.java
@@ -0,0 +1,14 @@
+package imageviewer_1;
+
+import imageviewer_1.view.ui.MainFrame;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        new Main().execute();
+    }
+
+    private void execute() {
+        MainFrame mainFrame = new MainFrame();
+    }
+}
